697db2e424a15293de2abb474cd6e6c21414996e
Training_C2a.05.01 <Training_C2a.05.01@CDC2-D-4T5NX52.dir.svc.accenture.com>
2019-04-29 11:53:53 +0530

    dfgh

--- Bold/src/test/java/Casestudy1/Runner.java
+++ Bold/src/test/java/Casestudy1/Runner.java
@@ -2,8 +2,9 @@ package Casestudy1;
 
 import org.junit.runner.RunWith;
 
+import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
-
+@CucumberOptions(plugin={"pretty","json:test/java/Casestudy1/target/cucumber.json"})
 @RunWith(Cucumber.class)
 public class Runner {
 
